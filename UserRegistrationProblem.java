ca6909353503087c09ec36ca2b5061638d6fc1fb
ppgupta <premprakashnayak69@gmail.com>
2020-09-30 00:16:00 +0530

    Lastname UC2

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -12,16 +12,35 @@ public class UserRegistrationProblem {
 		Matcher m=p.matcher(firstName);
 		return m.matches();
 	}
+	public static boolean isValidLastName(String lastName ) {
+		String regex="^[A-Z]{1}[a-z]{2,}";
+		Pattern p=Pattern.compile(regex);
+		if(lastName==null) {
+			return false;
+		}
+		Matcher m=p.matcher(lastName);
+		return m.matches();
+	}
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter your first name: ");
 	String firstName=sc.nextLine();
 	
-	if(isValidFirstName(firstName)==true) {
+	if(isValidFirstName(firstName)) {
 		System.out.println("Valid firstname");
 	}
 	else {
 		System.out.println("Invalid first name");
+	}
+	//UC2
+	System.out.println("Enter your Last name: ");
+	String lastName=sc.nextLine();
+	
+	if(isValidLastName(lastName)) {
+		System.out.println("Valid lastname");
+	}
+	else {
+		System.out.println("Invalid last name");
 	}
 }
 }
